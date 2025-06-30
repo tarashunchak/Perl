c90829f1d0e02d9b064b380ded3bf08cf00007dd
tarashunchak <taras.gunchak@kpk-lp.com.ua>
2025-06-30 17:34:59 +0300

    Add files via upload

--- perl.pl
+++ perl.pl
@@ -1,15 +1,20 @@
 #!D:/Strawberry/perl/bin/perl.exe
 
 use strict;
+use warnings;
+use CGI;
 
 print "Content-type: text/html\n\n";
-print "Hello, world!";
+#print "Hello, world!";
 
-sub main{
-  my $file;
-  open($file, '>', 'file.txt') or die "Cannot open file";
-  print $file "Якісь дані";
-  close($file) or die "Cannot close file";
-}
+my $data = CGI->new;
+my $file;
+open($file, '>>', 'file.txt') or die "Cannot open file";
+print $file "\n-----------------------------------------\n";
+print $file "UserID: ".$data->param('user_id')."\n";
+print $file "New name: ".$data->param('name')."\n";
+print $file "New email: ".$data->param('email')."\n";
+print $file "New city: ".$data->param('city')."\n";
+print $file "\n-----------------------------------------\n";
 
-main();
+close($file) or die "Cannot close file";
---
+++ script.js
@@ -0,0 +1,38 @@
+const modal = new bootstrap.Modal(document.getElementById('editModal'));
+const nameInput = document.getElementById('nameInput');
+const cityInput = document.getElementById('cityInput');
+const emailInput = document.getElementById('emailInput');
+const cardIndexInput = document.getElementById('cardIndex');
+
+document.querySelectorAll('.edit-btn').forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+    const card = btn.closest('.card');
+    const name = card.querySelector('.user-name').textContent;
+    const city = card.querySelector('.user-city').textContent;
+    const email = card.querySelector('.user-email').textContent;
+    
+    const formData = new FormData();
+    formData.append("user_id", index);
+    formData.append("name", nameInput.value);
+    formData.append("city", cityInput.value);
+    formData.append("email", emailInput.value);
+
+    fetch("./perl.pl",{ method: "POST", body: formData});
+
+    nameInput.value = name;
+    cityInput.value = city;
+    emailInput.value = email;
+    cardIndexInput.value = index;
+
+    modal.show();
+    });
+});
+
+document.getElementById('saveBtn').addEventListener('click', () => {
+    const index = cardIndexInput.value;
+    const card = document.querySelectorAll('.card')[index];
+    card.querySelector('.user-name').textContent = nameInput.value;
+    card.querySelector('.user-city').textContent = cityInput.value;
+    card.querySelector('.user-email').textContent = emailInput.value;
+    modal.hide();
+});
